20077ebe6d0917c1306adb8f5c4dd16c7a8ae9d9
William <mack.mindenhall@gmail.com>
2020-08-03 12:35:12 +0000

    Added button and DayListItem components with respective stories

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
+import classnames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  const btnClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button
+      disabled={props.disabled}
+      className={btnClass}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
