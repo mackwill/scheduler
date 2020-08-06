1a9fe4fbc86cf22d904ee2bec286e78afaf02523
Will <mack.mindenhall@gmail.com>
2020-08-06 12:27:41 -0400

    Refactored useApplicationData to use useReducer hook instead of useState'

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 import { findDayByName, updateDaysArray } from "helpers/selectors";
 
 export default function useApplicationData() {
-  const [state, setState] = useState({
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {},
+  //   spots: 0,
+  // });
+
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case SET_DAY: {
+        return { ...state, day: action.value };
+      }
+      case SET_APPLICATION_DATA: {
+        return { ...state, ...action.value };
+      }
+      case SET_INTERVIEW: {
+        console.log("action-value", action.value);
+        return { ...state, ...action.value };
+      }
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type: ${action.type}`
+        );
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
@@ -12,7 +43,8 @@ export default function useApplicationData() {
   });
 
   // Set the state for the day and days
-  const setDay = (day) => setState({ ...state, day });
+  // const setDay = (day) => setState({ ...state, day });
+  const setDay = (day) => dispatch({ type: SET_DAY, value: day });
 
   useEffect(() => {
     Promise.all([
@@ -20,13 +52,22 @@ export default function useApplicationData() {
       Promise.resolve(axios.get("/api/appointments")),
       Promise.resolve(axios.get("/api/interviewers")),
     ]).then((all) => {
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-        spots: findDayByName(all[0].data, state.day)[0].spots,
-      }));
+      // setState((prev) => ({
+      //   ...prev,
+      //   days: all[0].data,
+      //   appointments: all[1].data,
+      //   interviewers: all[2].data,
+      //   spots: findDayByName(all[0].data, state.day)[0].spots,
+      // }));
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        value: {
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+          spots: findDayByName(all[0].data, state.day)[0].spots,
+        },
+      });
     });
   }, []); //An empty array is passed as a dependancy to avoid an infinite loop of the request beign made since there is no real dependancy
 
@@ -56,11 +97,18 @@ export default function useApplicationData() {
     const days = updateDaysArray(state.days, state.day, day);
 
     return axios.put(`/api/appointments/${id}`, appointment).then((res) => {
-      setState((prev) => ({
-        ...prev,
-        appointments,
-        days,
-      }));
+      // setState((prev) => ({
+      //   ...prev,
+      //   appointments,
+      //   days,
+      // }));
+      dispatch({
+        type: SET_INTERVIEW,
+        value: {
+          appointments,
+          days,
+        },
+      });
     });
   };
 
@@ -91,11 +139,18 @@ export default function useApplicationData() {
     const days = updateDaysArray(state.days, state.day, day);
 
     return axios.delete(`/api/appointments/${id}`, appointment).then(() => {
-      setState((prev) => ({
-        ...prev,
-        appointments,
-        days,
-      }));
+      // setState((prev) => ({
+      //   ...prev,
+      //   appointments,
+      //   days,
+      // }));
+      dispatch({
+        type: SET_INTERVIEW,
+        value: {
+          appointments,
+          days,
+        },
+      });
     });
   };
 
